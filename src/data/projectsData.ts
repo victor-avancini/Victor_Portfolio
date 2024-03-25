d4d041ae2bd651b2afa960e07f04cafb48d89f1a
Victor Avancini <victor.avancini@hotmail.com>
2024-03-25 17:50:44 -0300

    Portfólio updated

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -2,6 +2,9 @@ import { Project } from "../interfaces/projects.interfaces";
 import KenzieHub from "../assets/KenzieHub.png";
 import Portfólio from "../assets/Portfólio.png";
 import BurguerKenzie from "../assets/BurguerKenzie.png";
+import OpenMusic from "../assets/Open_Music.png";
+import KenzieFlix from "../assets/KenzieFlix.png";
+import GenericSite from "../assets/Generic_Site.png";
 
 export const projects: Project[] = [
   {
@@ -20,9 +23,37 @@ export const projects: Project[] = [
     liveUrl: "https://burguer-kenzie-victor-avancini.vercel.app/",
     githubUrl: "link-do-github-aqui",
   },
+
   {
     id: 3,
-    name: "Portfólio genérico",
+    name: "Open Music",
+    description: "Descrição do Projeto",
+    imageUrl: OpenMusic,
+    liveUrl: "https://open-music-victor-avancini.vercel.app/",
+    githubUrl: "link-do-github-aqui",
+  },
+
+  {
+    id: 4,
+    name: "Kenzie Flix",
+    description: "Descrição do Projeto",
+    imageUrl: KenzieFlix,
+    liveUrl: "https://kenzie-flix-victor-avancini.vercel.app/",
+    githubUrl: "link-do-github-aqui",
+  },
+
+  {
+    id: 5,
+    name: "Generic Site",
+    description: "Descrição do Projeto",
+    imageUrl: GenericSite,
+    liveUrl: "https://generic-site-victor-avancini.vercel.app/",
+    githubUrl: "link-do-github-aqui",
+  },
+  
+  {
+    id: 6,
+    name: "Generic portfolio",
     description: "Descrição do Projeto 2",
     imageUrl: Portfólio,
     liveUrl: "https://portfolio-template-victor-avancini.vercel.app/",
