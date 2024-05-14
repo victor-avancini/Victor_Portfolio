a8fa569cf6419e58c2cfb880f2d017321837d664
Victor Avancini <victor.avancini@hotmail.com>
2024-05-14 16:11:23 -0300

    Update ProjectCard.tsx

--- src/components/ProjectsCard/ProjectCard.tsx
+++ src/components/ProjectsCard/ProjectCard.tsx
@@ -7,16 +7,18 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     return (
         <div className={styles.project_card}>
             <a href={project.liveUrl} target="_blank"><img src={project.imageUrl} alt={project.name} /></a>
-            <div className={styles.links}>
+            <div className={styles.liveUrl}>
                 <a href={project.liveUrl} target="_blank">{project.name}</a>
                 <div className={styles.technologies}>
                     {project.technologies.map((tech: Technology) => (
                         <i className={tech.link}></i>
                     ))}
                 </div>
-                <a href={project.githubUrl} target="_blank">
-                    <i className="devicon-github-original"></i>
-                </a>
+                <div className={styles.githubUrl}>
+                    <a href={project.githubUrl} target="_blank">
+                        <i className="devicon-github-original"></i>
+                    </a>
+                </div>
             </div>
         </div>
     )
